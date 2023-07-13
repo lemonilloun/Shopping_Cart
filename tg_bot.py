c0a34b03ce77fcb372da83b7f6fce81536c5fd1d
lemonilloun <alekssyes4u@gmail.com>
2023-07-13 18:40:08 +0300

    regist list of product

--- tg_bot.py
+++ tg_bot.py
@@ -1,27 +1,49 @@
 import telebot
 from telebot import types
 from tg_token import token
+
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
-    prev_message = ''
+    curr_cart_id = 0
+    person = []
+    global shop_list
+
+    def cart_creation(message):
+        person.append(message.text)
+        bot.send_message(message.chat.id, f'Создание списка покупок для пользователя {person[-1]}')
+        msg = bot.send_message(message.chat.id,
+                         "Напишите ниже список покупок для пользователя.\n\nПришлите список в формате: \n<название продукта>-<стоимость> -<количество> ( по умолчанию количество равно 1)\n\nПример1: Молоко-67\nПример2: Хлеб-42-3",)
+        bot.register_next_step_handler(msg, cart_packing)
+
+
+
+    def cart_packing(message):
+        #shop_list += message.text
+        if message.text == "Завершить список покупок для пользователя":
+            pass
+        else:
+            print(message.text)
+            rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            rmk.add(types.KeyboardButton("Завершить список покупок для пользователя"))
+            msg = bot.send_message(message.chat.id, "Нажмите на кнопку для завершения списка", reply_markup=rmk)
+            bot.register_next_step_handler(msg, cart_packing)
+
+
+
     @bot.message_handler(commands= ['shop', 'shopping'])
     def working(message):
-        markup_inline = types.InlineKeyboardMarkup()
-        item_create_cart = types.InlineKeyboardButton(text="Создать",
-                                               callback_data = 'create')
-        item_change_cart = types.InlineKeyboardButton(text = "Редактировать",
-                                                      callback_data = 'change')
-
+        markup_inline = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item_create_cart = types.KeyboardButton("Создать")
+        item_change_cart = types.KeyboardButton("Редактировать")
         markup_inline.add(item_create_cart, item_change_cart)
-        bot.send_message(message.chat.id, "Что бы вы хотели сделать c корзиной?",
+        msg = bot.send_message(message.chat.id, "Что бы вы хотели сделать c корзиной?",
                          reply_markup = markup_inline)
+        bot.register_next_step_handler(msg, cart_answer)
 
-    @bot.callback_query_handler(func=lambda call: True)
-    def cart_answer(call):
-        if call.data == 'create':
-            prev_message = 'create'
-            bot.send_message(call.message.chat.id, 'Напишите пользователя')
-
+    def cart_answer(message):
+        if message.text == "Создать":
+            msg = bot.send_message(message.chat.id, 'Напишите пользователя')
+            bot.register_next_step_handler(msg, cart_creation)
 
 
 
